100f9a57722ee6da6f053941dc8616fe7b593ebe
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-11-04 13:06:35 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,53 +2,90 @@ import React from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [data, setdata] = React.useState([]);
-  function handleclick() {
-    let val = document.getElementById("task").value;
-    document.getElementById("task").value = "";
-    if (val === "") return;
-    let arr = [...data];
-    let obj = { data1: val, show: false };
-    arr.push(obj);
-    setdata(arr);
-  }
-  function handleedit(i) {
-    let temp = [...data];
-    temp[i].show = true;
-    setdata(temp);
-  }
-  function handledelete(index) {
-    let temp = [...data];
-    temp.splice(index, 1);
-    setdata(temp);
-  }
-  function handlesave(index) {
-    let temp = [...data];
-    if (document.getElementById("ed").value === "") return;
-    temp[index].show = false;
-    temp[index].data1 = document.getElementById("ed").value;
-    setdata(temp);
-  }
+  const [tasks, setTasks] = React.useState([]);
+  const task = React.useRef();
+  const [taskString, setTaskString] = React.useState("");
+  const [editedTaskString, setEditedTaskString] = React.useState("");
+  const editedTask = React.useRef();
+  const [selectedTask, setSelectedTask] = React.useState(null);
+
+  const saveTask = () => {
+    if (taskString === "") return;
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        id: taskString + tasks.length,
+        name: taskString
+      }
+    ]);
+    setTaskString("");
+  };
+
+  const editTask = (id) => {
+    if (editedTaskString === "") return;
+    let tasksRep = tasks.map((task) => {
+      let newTask = { ...task };
+      if (task.id === id) newTask.name = editedTaskString;
+      return newTask;
+    });
+    setTasks((tasks) => tasksRep);
+    setSelectedTask(null);
+    setEditedTaskString("");
+  };
+
+  const deleteTask = (id) => {
+    let tasksRep = tasks.filter((task) => task.id !== id);
+    setTasks((tasks) => tasksRep);
+  };
+
+  const edit = (id) => {
+    setSelectedTask(id);
+  };
+
+  const handleTaskChange = (e) => {
+    let value = e.target.value;
+    setTaskString(value);
+  };
+
+  const handleEdit = (e) => {
+    let value = e.target.value;
+    setEditedTaskString(value);
+  };
+
   return (
     <div id="main">
-      <input id="task" type="text"></input>
-      <button id="btn" onClick={handleclick}>
-        Add
+      <textarea
+        id="task"
+        value={taskString}
+        onChange={handleTaskChange}
+        ref={task}
+        rows="4"
+        cols="50"
+      ></textarea>
+      <button id="btn" onClick={saveTask}>
+        save
       </button>
       <ol>
-        {data.map((ele, index) => (
-          <li key={index} className="list">
-            {ele.data1}
-            <button onClick={() => handleedit(index)} className="edit">
-              edit
-            </button>
-            <button onClick={() => handledelete(index)} className="delete">
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span className="list">{task.name}</span>
+            <button className="delete" onClick={() => deleteTask(task.id)}>
               delete
             </button>
-            {ele.show === true ? (
+            <button className="edit" onClick={() => edit(task.id)}>
+              edit
+            </button>
+            {selectedTask === task.id ? (
               <>
-                <input type="text" id="ed" className="editTask"></input>
-                <button onClick={() => handlesave(index)} className="saveTask">
+                <textarea
+                  ref={editedTask}
+                  value={editedTaskString}
+                  onChange={handleEdit}
+                  className="editTask"
+                  rows="4"
+                  cols="50"
+                ></textarea>
+                <button className="saveTask" onClick={() => editTask(task.id)}>
                   save
                 </button>
               </>
